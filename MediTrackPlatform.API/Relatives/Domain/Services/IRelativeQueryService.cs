a6f7b1f19d3f3004ed4959b69f29e4e188b28856
MarceBq <marcebq2005@gmail.com>
2025-11-08 20:46:18 -0500

    feat(relatives):  add query service to relatives bounded context

---
+++ MediTrackPlatform.API/Relatives/Domain/Services/IRelativeQueryService.cs
@@ -0,0 +1,8 @@
+﻿
+using MediTrackPlatform.API.Relatives.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Relatives.Domain.Model.Queries;
+
+public interface IRelativeQueryService
+{
+    Task<Relative?> Handle(GetRelativesByIdQuery query);
+}
