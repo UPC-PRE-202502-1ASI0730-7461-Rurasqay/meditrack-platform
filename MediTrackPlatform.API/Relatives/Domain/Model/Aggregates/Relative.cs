a90b5a9b111e151bffc5e9e59faa96d3a7fbb85b
MarceBq <marcebq2005@gmail.com>
2025-11-08 20:45:48 -0500

    feat(relatives):  add agregates to bounded context relatives

---
+++ MediTrackPlatform.API/Relatives/Domain/Model/Aggregates/Relative.cs
@@ -0,0 +1,18 @@
+﻿namespace MediTrackPlatform.API.Relatives.Domain.Model.Aggregates;
+
+public partial class Relative
+{
+    public int Id { get; private set; }
+    public string Plan { get; private set; } = string.Empty;
+    public string FirstName { get; private set; } = string.Empty;
+    public string LastName { get; private set; } = string.Empty;
+    public string PhoneNumber { get; private set; } = string.Empty;
+    public string ProfileImage { get; private set; } = string.Empty;
+    public int SeniorCitizenId { get; private set; }
+    
+    public SeniorCitizen SeniorCitizen { get; private set; }
+    
+    public Relative()
+    {
+    }
+}
