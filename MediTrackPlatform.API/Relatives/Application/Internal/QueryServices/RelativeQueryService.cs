8c0c22607bb58f004655cf10e98c39c68d7cfdce
MarceBq <marcebq2005@gmail.com>
2025-11-08 20:49:01 -0500

    feat(relatives): add query service to application file for relatives bounded context

---
+++ MediTrackPlatform.API/Relatives/Application/Internal/QueryServices/RelativeQueryService.cs
@@ -0,0 +1,15 @@
+﻿using MediTrackPlatform.API.Relatives.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Relatives.Domain.Model.Queries;
+using MediTrackPlatform.API.Relatives.Domain.Repositories;
+
+namespace MediTrackPlatform.API.Relatives.Application.Internal.QueryServices;
+
+public class RelativeQueryService( IRelativeRepository  relativeRepository) : IRelativeQueryService
+{
+     
+
+    public Task<Relative?> Handle(GetRelativesByIdQuery query)
+    {
+        return relativeRepository.GetByIdAsync(query.id);
+    }
+}
