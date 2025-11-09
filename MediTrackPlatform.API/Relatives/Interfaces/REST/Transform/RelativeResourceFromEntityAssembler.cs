6598b882f255918a1220061d1767c588694bb1c1
MarceBq <marcebq2005@gmail.com>
2025-11-08 20:47:19 -0500

    feat(relatives):  add transform to relatives bounded context

---
+++ MediTrackPlatform.API/Relatives/Interfaces/REST/Transform/RelativeResourceFromEntityAssembler.cs
@@ -0,0 +1,33 @@
+﻿using MediTrackPlatform.API.Relatives.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Relatives.Interfaces.REST.Resources;
+
+namespace MediTrackPlatform.API.Relatives.Interfaces.REST.Transform;
+
+public class RelativeResourceFromEntityAssembler
+{
+    public static RelativeResource ToResourceFromEntity(Relative entity)
+    {
+        var seniorCitizenResource = new SeniorCitizenResource(
+            entity.SeniorCitizen.SeniorCitizenId,
+            entity.SeniorCitizen.FirstName,
+            entity.SeniorCitizen.LastName,
+            entity.SeniorCitizen.Dni,
+            entity.SeniorCitizen.Gender,
+            entity.SeniorCitizen.Height,
+            entity.SeniorCitizen.Weight,
+            entity.SeniorCitizen.BirthDate,
+            entity.SeniorCitizen.ProfileImage,
+            entity.SeniorCitizen.DeviceId
+        );
+
+        return new RelativeResource(
+            entity.Id,
+            entity.Plan,
+            entity.FirstName,
+            entity.LastName,
+            entity.PhoneNumber,
+            entity.ProfileImage,
+            seniorCitizenResource
+        );
+    }
+}
