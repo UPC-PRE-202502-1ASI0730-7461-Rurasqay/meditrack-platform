1255cb8f4a4d700e3e1d9705020136aa4e833fd6
MarceBq <marcebq2005@gmail.com>
2025-11-08 20:47:48 -0500

    feat(relatives):  add resources to entities for relatives bounded context

---
+++ MediTrackPlatform.API/Relatives/Interfaces/REST/Resources/RelativeResource.cs
@@ -0,0 +1,11 @@
+﻿namespace MediTrackPlatform.API.Relatives.Interfaces.REST.Resources;
+
+public record RelativeResource(
+    int Id,
+    string Plan,
+    string FirstName,
+    string LastName,
+    string PhoneNumber,
+    string ProfileImage,
+    SeniorCitizenResource SeniorCitizen
+);
