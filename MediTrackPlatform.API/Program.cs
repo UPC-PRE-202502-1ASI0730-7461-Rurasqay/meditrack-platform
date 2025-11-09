4c0fce0833d318163d99bc46b9934875ce8d2d00
MarceBq <marcebq2005@gmail.com>
2025-11-08 20:49:18 -0500

    fix(relatives): add relatives builder

--- MediTrackPlatform.API/Program.cs
+++ MediTrackPlatform.API/Program.cs
@@ -1,3 +1,6 @@
+using MediTrackPlatform.API.Relatives.Application.Internal.QueryServices;
+using MediTrackPlatform.API.Relatives.Domain.Repositories;
+using MediTrackPlatform.API.Relatives.Infrastructure.Persistence.Repositories;
 using MediTrackPlatform.API.Shared.Domain.Repositories;
 using MediTrackPlatform.API.Shared.Infrastructure.Interfaces.ASP.Configuration;
 using MediTrackPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
@@ -177,6 +180,8 @@ else if (builder.Environment.IsProduction())
 
 // Shared Bounded Context Injection Configuration
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IRelativeQueryService, RelativeQueryService>();
+builder.Services.AddScoped<IRelativeRepository, RelativeRepository>();
 
 var app = builder.Build();
 
