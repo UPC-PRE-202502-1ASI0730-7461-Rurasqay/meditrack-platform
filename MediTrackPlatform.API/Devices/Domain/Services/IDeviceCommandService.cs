cc279c92ac00dc0727cb0e3cc776e6ae0ef09c88
Franco Rioja <diegoriojanu@gmail.com>
2025-11-10 02:12:49 -0500

    feat(devices): add Handle methods to IAlertCommandService and IDeviceCommandService

--- MediTrackPlatform.API/Devices/Domain/Services/IDeviceCommandService.cs
+++ MediTrackPlatform.API/Devices/Domain/Services/IDeviceCommandService.cs
@@ -1,6 +1,9 @@
+using MediTrackPlatform.API.Devices.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Devices.Domain.Model.Commands;
+
 namespace MediTrackPlatform.API.Devices.Domain.Services;
 
 public interface IDeviceCommandService
 {
-    
+    Task<Device?> Handle(CreateDeviceCommand command);
 }
