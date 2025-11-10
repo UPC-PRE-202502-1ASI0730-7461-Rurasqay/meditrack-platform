b7a6994007bba0ec56bead0fffb311488abdc54c
Franco Rioja <diegoriojanu@gmail.com>
2025-11-10 02:21:36 -0500

    feat(devices): implement AlertQueryService and DeviceQueryService with Handle methods

--- MediTrackPlatform.API/Devices/Application/QueryServices/DeviceQueryService.cs
+++ MediTrackPlatform.API/Devices/Application/QueryServices/DeviceQueryService.cs
@@ -1,6 +1,19 @@
+using MediTrackPlatform.API.Devices.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Devices.Domain.Model.Queries;
+using MediTrackPlatform.API.Devices.Domain.Repositories;
+using MediTrackPlatform.API.Devices.Domain.Services;
+
 namespace MediTrackPlatform.API.Devices.Application.QueryServices;
 
-public class DeviceQueryService
+public class DeviceQueryService(IDeviceRepository deviceRepository) : IDeviceQueryService
 {
-    
+    public async Task<Device?> Handle(GetDeviceByIdQuery query)
+    {
+        return await deviceRepository.FindByIdAsync(query.DeviceId);
+    }
+
+    public async Task<IEnumerable<Device>> Handle(GetAllDevicesQuery query)
+    {
+        return await deviceRepository.ListByHolderIdAsync(query.HolderId);
+    }
 }
