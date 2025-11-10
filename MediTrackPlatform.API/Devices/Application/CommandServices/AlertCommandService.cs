a5d9818888a92bc6a27f8fda9b3d494fedae9a3f
Franco Rioja <diegoriojanu@gmail.com>
2025-11-10 02:13:00 -0500

    feat(devices): implement Handle methods in AlertCommandService and DeviceCommandService

---
+++ MediTrackPlatform.API/Devices/Application/CommandServices/AlertCommandService.cs
@@ -0,0 +1,37 @@
+﻿using MediTrackPlatform.API.Devices.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Devices.Domain.Model.Commands;
+using MediTrackPlatform.API.Devices.Domain.Repositories;
+using MediTrackPlatform.API.Devices.Domain.Services;
+using MediTrackPlatform.API.Shared.Domain.Repositories;
+
+namespace MediTrackPlatform.API.Devices.Application.CommandServices;
+
+public class AlertCommandService(IAlertRepository alertRepository, IUnitOfWork unitOfWork) 
+    : IAlertCommandService
+{
+    public async Task<Alert?> Handle(CreateAlertCommand command)
+    {
+        var alert = new Alert
+        {
+            DeviceId = command.DeviceId.ToString(),
+            EAlertType = command.Type,
+            Message = command.Message,
+            DataRegistered = command.DataRegistered,
+            RegisteredAt = command.RegisteredAt
+        };
+
+        try
+        {
+            await alertRepository.AddAsync(alert);
+            
+            await unitOfWork.CompleteAsync();
+            
+            return alert;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred while creating the alert: {e.Message}");
+            return null;
+        }
+    }
+}
--- MediTrackPlatform.API/Devices/Application/CommandServices/DeviceCommandService.cs
+++ MediTrackPlatform.API/Devices/Application/CommandServices/DeviceCommandService.cs
@@ -1,6 +1,30 @@
+using MediTrackPlatform.API.Devices.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Devices.Domain.Model.Commands;
+using MediTrackPlatform.API.Devices.Domain.Repositories;
+using MediTrackPlatform.API.Devices.Domain.Services;
+using MediTrackPlatform.API.Shared.Domain.Repositories;
+
 namespace MediTrackPlatform.API.Devices.Application.CommandServices;
 
-public class DeviceCommandService
+public class DeviceCommandService(IDeviceRepository deviceRepository, IUnitOfWork unitOfWork) 
+    : IDeviceCommandService
 {
-    
+    public async Task<Device?> Handle(CreateDeviceCommand command)
+    {
+        var device = new Device(command.Model, command.HolderId.ToString());
+        
+        try
+        {
+            await deviceRepository.AddAsync(device);
+            
+            await unitOfWork.CompleteAsync();
+            
+            return device;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred while creating the device: {e.Message}");
+            return null;
+        }
+    }
 }
