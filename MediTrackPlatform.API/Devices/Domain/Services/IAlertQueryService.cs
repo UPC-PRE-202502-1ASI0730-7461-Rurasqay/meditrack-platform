a828518f58c70cf1610a792c4b85b20822377145
Franco Rioja <diegoriojanu@gmail.com>
2025-11-10 02:21:27 -0500

    feat(devices): add Handle methods to IAlertQueryService and IDeviceQueryService

--- MediTrackPlatform.API/Devices/Domain/Services/IAlertQueryService.cs
+++ MediTrackPlatform.API/Devices/Domain/Services/IAlertQueryService.cs
@@ -1,6 +1,11 @@
+using MediTrackPlatform.API.Devices.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Devices.Domain.Model.Queries;
+
 namespace MediTrackPlatform.API.Devices.Domain.Services;
 
 public interface IAlertQueryService
 {
+    Task<Alert?> Handle(GetAlertByIdQuery query);
     
+    Task<IEnumerable<Alert>> Handle(GetAllAlertsQuery query);
 }
--- MediTrackPlatform.API/Devices/Domain/Services/IDeviceQueryService.cs
+++ MediTrackPlatform.API/Devices/Domain/Services/IDeviceQueryService.cs
@@ -1,6 +1,11 @@
+using MediTrackPlatform.API.Devices.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Devices.Domain.Model.Queries;
+
 namespace MediTrackPlatform.API.Devices.Domain.Services;
 
 public interface IDeviceQueryService
 {
+    Task<Device?> Handle(GetDeviceByIdQuery query);
     
+    Task<IEnumerable<Device>> Handle(GetAllDevicesQuery query);
 }
