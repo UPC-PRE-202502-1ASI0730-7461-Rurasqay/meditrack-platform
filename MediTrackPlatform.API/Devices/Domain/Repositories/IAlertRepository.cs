4f204338141150fc087842d98913645cdb8c9e97
Franco Rioja <diegoriojanu@gmail.com>
2025-11-09 19:42:03 -0500

    feat(devices): enhance repositories with methods for alert and device management

--- MediTrackPlatform.API/Devices/Domain/Repositories/IAlertRepository.cs
+++ MediTrackPlatform.API/Devices/Domain/Repositories/IAlertRepository.cs
@@ -1,6 +1,11 @@
 namespace MediTrackPlatform.API.Devices.Domain.Repositories;
 
+using MediTrackPlatform.API.Devices.Domain.Model.Aggregates;
 public interface IAlertRepository
 {
+    Task AddAsync(Alert alert);
     
+    Task<Alert?> FindByIdAsync(int alertId);
+    
+    Task<IEnumerable<Alert>> ListBySeniorCitizenIdAsync(int seniorCitizenId);
 }
--- MediTrackPlatform.API/Devices/Domain/Repositories/IDeviceRepository.cs
+++ MediTrackPlatform.API/Devices/Domain/Repositories/IDeviceRepository.cs
@@ -1,6 +1,15 @@
+using MediTrackPlatform.API.Devices.Domain.Model.Aggregates;
+
 namespace MediTrackPlatform.API.Devices.Domain.Repositories;
 
 public interface IDeviceRepository
 {
+    Task AddAsync(Device device);
+
+    Task<Device?> FindByIdAsync(int deviceId);
+    
+    Task<IEnumerable<Device>> ListByHolderIdAsync(int holderId);
+    
+    void Update(Device device);
     
 }
