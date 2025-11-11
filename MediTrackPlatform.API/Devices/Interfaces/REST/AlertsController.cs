427d60eb55895fccd5712f2602599f005b309244
Franco Rioja <diegoriojanu@gmail.com>
2025-11-11 02:38:58 -0500

    feat(devices): implement alert retrieval endpoints in AlertsController

--- MediTrackPlatform.API/Devices/Interfaces/REST/AlertsController.cs
+++ MediTrackPlatform.API/Devices/Interfaces/REST/AlertsController.cs
@@ -1,6 +1,70 @@
-namespace MediTrackPlatform.API.Devices.Interfaces.REST;
+using MediTrackPlatform.API.Devices.Domain.Model.Queries; 
+using MediTrackPlatform.API.Devices.Domain.Services;     
+using MediTrackPlatform.API.Devices.Interfaces.REST.Resources;
+using Microsoft.AspNetCore.Mvc;
 
-public class AlertsController
+
+namespace MediTrackPlatform.API.Devices.Interfaces.REST
 {
-    
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class AlertsController : ControllerBase
+    {
+        private readonly IAlertQueryService _alertQueryService;
+
+        public AlertsController(IAlertQueryService alertQueryService)
+        {
+            _alertQueryService = alertQueryService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<AlertResource>), 200)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetAllAlerts([FromQuery] int seniorCitizenId)
+        {
+            var query = new GetAllAlertsQuery(seniorCitizenId);
+            
+            var alerts = await _alertQueryService.Handle(query);
+            
+            var resources = alerts.Select(alert => 
+                new AlertResource(
+                    alert.AlertId,
+                    alert.DeviceId,
+                    alert.EAlertType.ToString(),
+                    alert.Message,
+                    alert.DataRegistered,
+                    alert.RegisteredAt
+                ));
+            
+            return Ok(resources);
+        }
+
+
+        [HttpGet("{alertId}")]
+        [ProducesResponseType(typeof(AlertResource), 200)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(500)]
+        public async Task<IActionResult> GetAlertById(int alertId)
+        {
+            var query = new GetAlertByIdQuery(alertId);
+            
+            var alert = await _alertQueryService.Handle(query);
+
+            if (alert == null)
+            {
+                return NotFound();
+            }
+
+            var resource = new AlertResource(
+                alert.AlertId,
+                alert.DeviceId,
+                alert.EAlertType.ToString(),
+                alert.Message,
+                alert.DataRegistered,
+                alert.RegisteredAt
+            );
+
+            return Ok(resource);
+        }
+    }
 }
