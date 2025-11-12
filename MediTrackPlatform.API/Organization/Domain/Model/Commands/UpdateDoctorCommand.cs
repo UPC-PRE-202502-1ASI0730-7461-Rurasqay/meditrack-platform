bad7eceb79f2aea218ac74f4ecb34906990f003f
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-11-12 00:12:03 -0500

    fix(command): removed unused attributes.

--- MediTrackPlatform.API/Organization/Domain/Model/Commands/UpdateDoctorCommand.cs
+++ MediTrackPlatform.API/Organization/Domain/Model/Commands/UpdateDoctorCommand.cs
@@ -2,8 +2,6 @@ namespace MediTrackPlatform.API.Organization.Domain.Model.Commands;
 
 public record UpdateDoctorCommand(
     int DoctorId,
-    int UserId,
-    int OrganizationId,
     string FirstName,
     string LastName,
     int Age,
