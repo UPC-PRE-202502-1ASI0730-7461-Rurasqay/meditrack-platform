8f34da6b22663310bd0c4d8c90fd61082d61da37
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-11-12 10:49:27 -0500

    feat(application): add admincommandservice for admin lifecycle operations.

---
+++ MediTrackPlatform.API/Organization/Application/Internal/CommandServices/AdminCommandService.cs
@@ -0,0 +1,43 @@
+using Cortex.Mediator;
+using MediTrackPlatform.API.Organization.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Organization.Domain.Model.Commands;
+using MediTrackPlatform.API.Organization.Domain.Model.Events;
+using MediTrackPlatform.API.Organization.Domain.Repositories;
+using MediTrackPlatform.API.Organization.Domain.Services;
+using MediTrackPlatform.API.Shared.Domain.Repositories;
+
+namespace MediTrackPlatform.API.Organization.Application.Internal.CommandServices;
+
+public class AdminCommandService(
+    IAdminRepository adminRepository,
+    IUnitOfWork unitOfWork,
+    IMediator domainEventPublisher)
+    : IAdminCommandService
+{
+    public async Task<int?> Handle(CreateAdminCommand command)
+    {
+        var admin = new Admin(command);
+        await adminRepository.AddAsync(admin);
+        await unitOfWork.CompleteAsync();
+
+        await domainEventPublisher.PublishAsync(new AdminCreatedEvent(admin.Id, admin.OrganizationId));
+        
+        return admin.Id;
+    }
+
+    public async Task<Admin?> Handle(UpdateAdminCommand command)
+    {
+        var result = adminRepository.FindByIdAsync(command.AdminId);
+        if (result.Result is null) throw new Exception(); // TODO: Use custom exceptions
+
+        var adminToUpdate = result.Result;
+        // TODO: Update Admin and return it!
+        return null;
+    }
+
+    public Task Handle(DeleteAdminCommand command)
+    {
+        // TODO: Implement this handle.
+        throw new NotImplementedException();
+    }
+}
