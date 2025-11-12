40fca0122a5317ef3f8f493fedea116ed2b2f18e
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-11-12 00:47:08 -0500

    feat(organization): add repository interfaces for each aggregate.

---
+++ MediTrackPlatform.API/Organization/Domain/Repositories/ICaregiverRepository.cs
@@ -0,0 +1,7 @@
+using MediTrackPlatform.API.Organization.Domain.Model.Aggregates;
+using MediTrackPlatform.API.Shared.Domain.Repositories;
+
+namespace MediTrackPlatform.API.Organization.Domain.Repositories;
+
+public interface ICaregiverRepository : IBaseRepository<Caregiver>
+{ }
---
+++ MediTrackPlatform.API/Organization/Domain/Repositories/IOrganizationRepository.cs
@@ -0,0 +1,8 @@
+using MediTrackPlatform.API.Shared.Domain.Repositories;
+
+namespace MediTrackPlatform.API.Organization.Domain.Repositories;
+
+public interface IOrganizationRepository : IBaseRepository<Model.Aggregates.Organization>
+{
+    
+}
