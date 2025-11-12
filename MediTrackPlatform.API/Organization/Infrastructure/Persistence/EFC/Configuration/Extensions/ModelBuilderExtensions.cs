666f67d6159a198e246ddf8e981181405ddb7dae
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-11-12 10:49:03 -0500

    feat(persistence): add model builder extensions for organizations configuration.

---
+++ MediTrackPlatform.API/Organization/Infrastructure/Persistence/EFC/Configuration/Extensions/ModelBuilderExtensions.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MediTrackPlatform.API.Organization.Infrastructure.Persistence.EFC.Configuration.Extensions;
+
+public static class ModelBuilderExtensions
+{
+    public static void ApplyOrganizationsConfiguration(this ModelBuilder builder)
+    {
+        // Organization Bounded Context
+        // TODO: Give structure to the configuration
+    }
+}
